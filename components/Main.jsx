297fe979783a8f6c648299ddbd6ff31892cbcf79
LeonardoR998 <leonardoricci839@gmail.com>
2024-11-28 15:39:44 +0100

    add main

---
+++ components/Main.jsx
@@ -0,0 +1,23 @@
+export default function () {
+  return (
+    <main>
+      <div className="container">
+        <div className="card">
+          <img src="" alt="" />
+          <div class="card-body">
+            <h5 className="card-title">Titolo del post</h5>
+            <p className="card-text">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Doloremque labore placeat nostrum atque. Quod dicta inventore
+              accusamus consequuntur itaque doloremque pariatur ad aut eos, fuga
+              alias nulla iste necessitatibus ipsam.
+            </p>
+            <a href="#" className="btn btn-warning text-uppercase text-white">
+              Leggi di più
+            </a>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
